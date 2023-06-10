cbfa7629be38a312cc41d7912ea376c7d6154b0f
LuizTrMr <luizfelipetmoraes@hotmail.com>
2023-06-10 01:45:52 -0300

    add something

--- src/routes/ik/+page.svelte
+++ src/routes/ik/+page.svelte
@@ -2,12 +2,19 @@
 </script>
 
 
+<div class="container">
+
+</div>
 <iframe src="/Kinematics/Kinematics.html" title="Iframe Test">
 </iframe>
 
 <style>
 	iframe {
 		display: flex;
+	}
+
+	.container {
+
 	}
 </style>
 
